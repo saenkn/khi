3b02530325296a48fd60a6f6380da221d3f67273
kyasbal <ikakeru@google.com>
2025-04-14 18:37:18 +0900

    Fix the generate-version.sh not to generate wrong lines (#137)
    when user build KHI multiple times

--- scripts/generate-version.sh
+++ scripts/generate-version.sh
@@ -14,5 +14,5 @@
 # limitations under the License.
 
 
-echo "export const VERSION=\"$(cat VERSION)\"" >> ./web/src/environments/version.prod.ts
-echo "export const VERSION=\"$(cat VERSION)@$(git log -1 --pretty=format:%h )\"" >> ./web/src/environments/version.dev.ts
+echo "export const VERSION=\"$(cat VERSION)\"" > ./web/src/environments/version.prod.ts
+echo "export const VERSION=\"$(cat VERSION)@$(git log -1 --pretty=format:%h )\"" > ./web/src/environments/version.dev.ts
